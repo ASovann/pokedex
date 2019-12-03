5b6f715af445f3ea1914e4484097eb7f730656c5
Arthur <arthurlacombe@gmail.com>
2019-12-03 17:05:01 +0100

    first commit

---
+++ PokemonMVVM/DAL/PokemonAPIDAL.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using PokemonMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonMVVM.DAL
+{
+    public static class PokemonAPIDAL
+    {
+        const string LOADALL_URL = "https://pokeapi.co/api/v2/";
+        public static async Task<List<Pokemon>> LoadPokemons()
+        {
+            WebClient wc = new WebClient();
+            byte[] data = await wc.DownloadDataTaskAsync(new Uri(LOADALL_URL));
+            string json = Encoding.UTF8.GetString(data);
+            PokemonAPIResult result = JsonConvert.DeserializeObject<PokemonAPIResult>(json);
+        }
+    }
+}
---
+++ PokemonMVVM/Models/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonMVVM.Models
+{
+    public class PokemonAPIResult
+    {
+        public int Count { get; set; }
+        public string Next { get; set; }
+        public string Previous { get; set; }
+        public List<Pokemon> Results { get; set; }
+    }
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public String Url { get; set; }
+        public PokemonImgData Sprites { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public class PokemonImgData
+        {
+            public string front_default { get; set; }
+        }
+
+    }
+}
